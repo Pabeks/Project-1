5d438038a6cc00829ad19c90a20e54cdfa37cac8
Paul Abekah <paulabekah@Pauls-MacBook-Air.local>
2024-08-12 01:00:06 -0500

    update withnew doman

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "paje-terraform"
-    key    = "infra"
+    key    = "infra/tfstate"
     region = "us-east-1"
   }
 }
--- infra/terraform/modules/RDS/rds.tf
+++ infra/terraform/modules/RDS/rds.tf
@@ -17,12 +17,12 @@ resource "aws_db_instance" "paje-rds" {
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
   # name                   = "pajedb"
   username               = var.db-username
   password               = var.db-password
-  parameter_group_name   = "default.mysql5.7"
+  parameter_group_name   = "default.mysql8.0"
   db_subnet_group_name   = aws_db_subnet_group.paje-rds.name
   skip_final_snapshot    = true
   vpc_security_group_ids = var.db-sg
